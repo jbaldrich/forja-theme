967707457b09b0174f9fb072955136187e7d53fb
Jaco Baldrich <jb@jacobaldrich.com>
2020-12-22 16:38:29 +0100

    add custom css

--- custom-functions.php
+++ custom-functions.php
@@ -1,5 +1,21 @@
 <?php
-function jbr_add_new_font() {
-    wp_enqueue_style( 'confortaa-google-fonts', '//fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap', array(), CHILD_THEME_VERSION );
-}
-add_action( 'wp_enqueue_scripts', 'jbr_add_new_font' );
+$addConfortaaFont = static function () {
+    wp_enqueue_style(
+        'confortaa-google-fonts',
+        '//fonts.googleapis.com/css2?family=Comfortaa:wght@600&display=swap',
+        [],
+        CHILD_THEME_VERSION
+    );
+};
+add_action('wp_enqueue_scripts', $addConfortaaFont);
+
+$addCustomStyles = static function () {
+    $fileName = '/custom.css';
+    wp_enqueue_style(
+        'forja-custom-styles',
+        get_stylesheet_directory_uri() . $fileName,
+        [],
+        filemtime(get_stylesheet_directory() . $fileName)
+    );
+};
+add_action('wp_enqueue_scripts', $addCustomStyles);
